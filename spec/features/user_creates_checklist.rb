23eabe2e995def9b21d44356fef9b70aef84bd2c
cantstopcoding <jonathan_koss@yahoo.com>
2021-10-26 11:41:48 -0400

    create User Create List test

---
+++ spec/features/user_creates_checklist.rb
@@ -0,0 +1,14 @@
+require "rails_helper" 
+
+feature "User create list" do 
+    scenario "successfully" do 
+        visit root_path
+
+        click_on "Add new item"
+        fill_in "Title", with: "Go shopping"
+        click_on "Submit"
+
+        expect(page).to have_css '.checklist li', text: "Go shopping"
+    end
+end
+
