27cc2416ce636204afe0cb3de0b0bfda9f4ad093
cantstopcoding <jonathan_koss@yahoo.com>
2021-11-01 23:36:48 -0400

    create checklists index and create action

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -1,8 +1,21 @@
 class ChecklistsController < ApplicationController
     def index 
+        @checklists = Checklist.all
     end
 
     def new
         @checklist = Checklist.new
     end
+
+    def create
+        Checklist.create(checklists_params)
+        redirect_to checklists_path
+    end
+
+    private
+
+    def checklists_params
+        params.require(:checklist).permit(:title)
+    end
+
 end
