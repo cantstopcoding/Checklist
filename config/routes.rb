ca2f879b4fb7dc392c65f22e5129ae56e629d57f
cantstopcoding <jonathan_koss@yahoo.com>
2021-11-01 23:33:54 -0400

    add index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "checklists#index"
+  
 
-  resources :checklists, only: [:new, :create]
+  resources :checklists, only: [:index, :new, :create]
 end
